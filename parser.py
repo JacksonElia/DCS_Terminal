37d72885e2f336df89f028e03558163061767e51
Traptricker <85963782+Traptricker@users.noreply.github.com>
2021-12-23 00:23:44 -0500

    Fixed convention mistakes

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
-'''
+"""
 parser.py: Responsible for parsing command arguments, may include further functions for formatting terminal text
 Contributors: Andrew Combs
-'''
+"""
 
 import types
 from ast import literal_eval
@@ -15,29 +15,28 @@ class Parser(object):
 
         for command in commands:
             self.add_command(*command)
-    
-    
+
     def add_command(self, command: str, function: types.FunctionType, args: list, flags: list):
-        '''
+        """
         Adds a command to the parser's local dictionary.
         command [str]: The name of the argument
         function [function]: The function the command passes arguments for
         args [list]: A list of types in order of how arguments should be passed
         flags [list]: A list of strings containing the name of boolean flag values
-        '''
+        """
         self.lookup[command] = (function, args, flags)
 
     # TODO: Make code more readable
     def parse(self, given: str) -> tuple:
-        '''
+        """
         Parses a string using the local parser's dictionary
         given [str]: input string to be parsed
-        '''
+        """
         nodes = given.split(" ")
         nodes = [n for n in nodes if n]
 
         cmd = nodes[0]
-        if cmd not in self.lookup.keys(): return ("ERROR", "COMMAND NOT FOUND", f"{cmd} DOES NOT EXIST")
+        if cmd not in self.lookup.keys(): return "ERROR", "COMMAND NOT FOUND", f"{cmd} DOES NOT EXIST"
         func = self.lookup[cmd]
 
         args = nodes[1:]
@@ -54,7 +53,7 @@ class Parser(object):
                 else:
                     parsed_args.append(t(literal_eval(arg)))
             except ValueError:
-                return ("ERROR", "MISMATCHED TYPES", f"{type(arg)} IS NOT {t}")
+                return "ERROR", "MISMATCHED TYPES", f"{type(arg)} IS NOT {t}"
 
         # Parsing flags (**kwargs)
         parsed_flags = {}
@@ -62,22 +61,22 @@ class Parser(object):
             if flag in func[2]:
                 parsed_flags[flag[2:]] = True
             else:
-                return ("ERROR", "FLAG NOT FOUND", f"FLAG {flag} DOES NOT EXIT IN FUNCTION")
-        
+                return "ERROR", "FLAG NOT FOUND", f"FLAG {flag} DOES NOT EXIT IN FUNCTION"
+
         info = (cmd, tuple(parsed_args), parsed_flags)
         return info
 
     # TODO: Better error checking/logging
     def execute(self, info=None):
-        '''
+        """
         Executes a function given info parameters
         info [tuple]: info required in order of (command_name, args, kwargs)
-        '''
+        """
         cmd = info[0]
 
         # Actual error logging needed here
         if cmd == "ERROR": return info
-        if cmd not in self.lookup.keys(): return ("ERROR", "COMMAND NOT FOUND", f"{cmd} DOES NOT EXIST")
+        if cmd not in self.lookup.keys(): return "ERROR", "COMMAND NOT FOUND", f"{cmd} DOES NOT EXIST"
 
         func = self.lookup[cmd]
         return func[0](*info[1], **info[2])
