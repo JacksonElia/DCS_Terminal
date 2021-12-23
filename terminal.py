17a8e4bd53f4020256eaf0b35b7a89325b66e90b
Andrew <58674899+iBrushC@users.noreply.github.com>
2021-12-22 22:32:01 -0500

    Added terminal.py

---
+++ terminal.py
@@ -0,0 +1,38 @@
+'''
+terminal.py: Responsible for GUI and input handling
+Contributors: Andrew Combs
+'''
+
+import os
+import sys
+import keyboard
+
+# TODO: actually code
+class Terminal(object):
+	
+    def __init__(self):
+        self.theme = None
+        self.buffer = None
+        return
+    
+    # Prompts user input
+    def prompt(self):
+        return
+    
+    # Prints at a location
+    def disp(self):
+        return
+    
+    # Logs using vital info
+    def log(self):
+        return
+    
+    # Draws a sprite at a location
+    def sprite_draw(self):
+        return
+        
+    # Draws a block of code with syntax highlighting
+    def code_block(self):
+        return
+        
+    
