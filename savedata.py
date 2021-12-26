4e8eabcc4e391825c8db0c1c672e5a8e9960185b
Andrew <mrandrewmcombs@gmail.com>
2021-12-26 14:24:30 -0500

    Added savedata.py functionality

--- savedata.py
+++ savedata.py
@@ -4,10 +4,35 @@ Contributors: Andrew Combs
 """
 
 import json
-import sys
+import errno
 import os
 
 
 class JSONManager(object):
     def __init__(self, fp: str=""):
+        if os.path.exists(fp): 
+            self.fp = fp + "\save.json"
+        else:
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), fp)
+            return
         
+        self.buffer = {}
+        dump = json.dumps(self.buffer)
+        with open(self.fp, "w") as jsonfile:
+            jsonfile.write(dump)
+        
+        return
+    
+    def read(self) -> dict:
+        with open(self.fp, "r") as jsonfile:
+            data = jsonfile.read()
+            self.buffer = json.loads(data)
+          
+        return self.buffer
+    
+    def write(self, data: dict):
+        self.buffer = data
+        dump = json.dumps(self.buffer)
+        with open(self.fp, "w") as jsonfile:
+            jsonfile.write(dump)
+    
